2a29c878acb35e1642e62502eb2e61d6581057f6
MrXbean <50227621+MrXbean@users.noreply.github.com>
2020-10-29 17:52:57 +0700

    Add files via upload

---
+++ mochi00.sh
@@ -0,0 +1,49 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Rev
+echo $red"WELCOME"
+echo
+sleep 2
+echo $cyan"SILAHKAN PILIH MENUNYA"
+echo
+echo $green"1.) BELAJAR NANO"
+echo
+echo $green"2.) BELAJAR SCRIP"
+echo
+echo $green"3.) CLOSE/EXIT"
+echo
+echo $blue
+read -p "PilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Rev
+echo $yellow"WA gw aja pak wkwkwk"
+sleep 2
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay Rev
+echo $yellow"Wa gw aja pak wkwkwk"
+sleep 2
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $purple
+figlet "Bye Bye Sob"
+echo $blue"see u"
+sleep 3
+exit
+fi
